b15eb1f58127407551ed6899519b5f267e193b89
Quang Ha <qth20@bu.edu>
2018-03-14 01:10:18 -0400

    Add ignore file and Laplace operator on regular grid

---
+++ LaplaceRegular.cpp
@@ -0,0 +1,20 @@
+// Performing Laplacian operator on a square grid
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+
+typedef vector<double> Vec;
+
+void Laplace(Vec &x);
+
+int main(int argc, char** argv)
+{
+  
+}
+
+void Laplace(Vec &x)
+{
+  // Laplace operator on a square grid
+}
