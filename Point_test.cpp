fd73f6b660b0f6f494c0ec112176fe3977b71e35
Quang Ha <qth20@bu.edu>
2018-03-25 01:31:06 -0400

    Add git ignore

--- Point_test.cpp
+++ Point_test.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "Point.hpp"
-int main() {
-    Point a(0, 0);
-    Point b(0, 5);
-    Point c(6, 0);
-    double d_ab = dist(b, c);
-    double ar_abc = area(a, b, c);
-    std::cout << a.x << '\t' << a.y << '\t' << d_ab << '\t' << ar_abc;
+
+int main()
+{
+  Point a(0, 0);
+  Point b(0, 5);
+  Point c(6, 0);
+  double d_ab = dist(b, c);
+  double ar_abc = area(a, b, c);
+  std::cout << a.x << '\t' << a.y << '\t' << d_ab << '\t' << ar_abc;
 }
