137453253827e8a58b9abb06fb430b89c9a390bc
Quang Ha <qth20@bu.edu>
2018-04-01 17:33:43 -0400

    Fix some formating

--- Grid2D.h
+++ Grid2D.h
@@ -1,19 +1,20 @@
-#ifndef GRID2D_HPP
-#define GRID2D_HPP
+#ifndef GRID2D_H
+#define GRID2D_H
+
 #include <vector>
 #include <tuple>
 #include "Point.h"
 
 class Grid2D {
 public:
-    Grid2D(Point& top_left, Point& bot_right, int Nx, int Ny);
-    ~Grid2D();
-
-    std::vector<std::vector<Point>> points;
-    /* std::vector<std::vector<double>> weights; */
-    int Nx, Ny;
-
-    Point& operator()(int i, int j);
+  Grid2D(Point& top_left, Point& bot_right, int Nx, int Ny);
+  ~Grid2D();
+  
+  std::vector<std::vector<Point>> points;
+  /* std::vector<std::vector<double>> weights; */
+  int Nx, Ny;
+  
+  Point& operator()(int i, int j);
 };
 
 /* void set_weights(Grid2D& g); */
--- Point.h
+++ Point.h
@@ -1,17 +1,18 @@
-#ifndef POINT_HPP
-#define POINT_HPP
+#ifndef POINT_H
+#define POINT_H
 
 #include <cmath>
 #include <stdlib.h>
 #include <iostream>
+
 class Point
 {
 public:
-    Point();
-    Point(double _x, double _y);
-    ~Point();
-    double x, y;
-    void set(double _x, double _y);
+  Point();
+  Point(double _x, double _y);
+  ~Point();
+  double x, y;
+  void set(double _x, double _y);
 };
 
 inline double distsq(const Point& a, const Point& b)
@@ -25,7 +26,7 @@ inline double area(const Point& a,
 		   const Point& b,
 		   const Point& c)
 {
-    return 0.5*std::fabs(a.x*b.y + b.x*c.y + c.x*a.y - a.x*c.y - c.x*b.y - b.x*a.y);
+  return 0.5*std::fabs(a.x*b.y + b.x*c.y + c.x*a.y - a.x*c.y - c.x*b.y - b.x*a.y);
 }
 
 #endif
