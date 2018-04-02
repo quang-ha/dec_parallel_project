643c8119a95d26e3ce52e89de3775213340bf2e9
Quang Ha <qth20@bu.edu>
2018-04-02 00:54:52 -0400

    Working on eigenvalue

--- Grid2D.h
+++ Grid2D.h
@@ -9,12 +9,17 @@ class Grid2D {
 public:
   Grid2D(Point& top_left, Point& bot_right, int Nx, int Ny);
   ~Grid2D();
-  
+
+  // Vector to store points
   std::vector<std::vector<Point>> points;
   /* std::vector<std::vector<double>> weights; */
   int Nx, Ny;
-  
+
+  // Get points from grid
   Point& operator()(int i, int j);
+
+  // Laplace matrix operatrion
+  void perform_op(double *x_in, double *y_out); 
 };
 
 /* void set_weights(Grid2D& g); */
