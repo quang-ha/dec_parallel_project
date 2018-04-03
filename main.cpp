1e084134f13d0d176e314da29f8814050fb699c5
Quang Ha <qth20@bu.edu>
2018-04-03 11:29:25 -0400

    Clean up print statement

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include <SymEigsSolver.h>
 #include <iostream>
 
-#define N 4 // Number of points in each direction
+#define N 10 // Number of points in each direction
 #define LOW 0.0 // Low boundary
 #define HIGH 1.0 // High boundary
 
@@ -18,41 +18,14 @@ int main(int argc, char* argv[])
   Point b(HIGH, HIGH);
   Grid2D g(a, b, N, N);
 
-  // for (int i=0; i<=N+1; i++)
-  //   for (int j=0; j<N+1; j++)
-  //       printf("Index i %d j %d and x-coord %f y-coord %f \n",
-  // 	       i, j, g(i, j).x, g(i, j).y);
-
-  // // Create array of pointers that point to more array
-  // double* x = new double[N*N];
-  // double* y = new double[N*N];
-
-  // for (int i=0; i<N; i++)
-  //   for (int j=0; j<N; j++)
-  //     x[i*N + j] = i*N + j;
-
-  // printf("x \n");
-  // for (int i=0; i<N*N; i++)
-  //   printf("%f ", x[i]);
-
-  // printf("\n");
-
-  // g.perform_op(x, y);
-
-  // printf("y \n");
-  // for (int i=0; i<N*N; i++)
-  //   printf("%f ", y[i]);
-
-  // printf("\n");
-
-  Spectra::SymEigsSolver<double, Spectra::LARGEST_ALGE, Grid2D> eigs(&g, 8, 16);
+  Spectra::SymEigsSolver<double, Spectra::LARGEST_ALGE, Grid2D> eigs(&g, 15, 16);
   eigs.init();
   eigs.compute();
   
   if(eigs.info() == Spectra::SUCCESSFUL)
     {
       Eigen::VectorXd evalues = eigs.eigenvalues();
-      std::cout << "Eigenvalues found:\n" << evalues << std::endl;
+      std::cout << "Eigenvalues found: \n" << evalues << std::endl;
     }
   
   return 0;
