ae78822ae6d109cc0d7257af18be34bac0f40654
Quang Ha <qth20@bu.edu>
2018-04-08 19:18:37 -0400

    Fix a scaling issue

--- plot_grid.py
+++ plot_grid.py
@@ -10,19 +10,20 @@ x, y = data[1:, 0], data[1:, 1]
 for i in range(Nx):
     for j in range(Ny):
         if ((i < Nx-1) and (j < Ny-1)):
-            plt.plot((x[i*Nx + j], x[(i+1)*Nx + j]),
-                     (y[i*Nx + j], y[i*Nx + j]), color='b') 
-            plt.plot((x[i*Nx + j], x[(i+1)*Nx + (j+1)]),
-                     (y[i*Nx + j], y[(i+1)*Nx + (j+1)]), color='b') 
-            plt.plot((x[i*Nx + j], x[i*Nx + (j+1)]),
-                     (y[i*Nx + j], y[i*Nx + (j+1)]), color='b')
+            plt.plot((x[i*Ny + j], x[(i+1)*Ny + j]),
+                     (y[i*Ny + j], y[i*Ny + j]), color='b') 
+            plt.plot((x[i*Ny + j], x[(i+1)*Ny + (j+1)]),
+                     (y[i*Ny + j], y[(i+1)*Ny + (j+1)]), color='b') 
+            plt.plot((x[i*Ny + j], x[i*Ny + (j+1)]),
+                     (y[i*Ny + j], y[i*Ny + (j+1)]), color='b')
         elif ((i < Nx-1) and (j == Ny-1)):
-            plt.plot((x[i*Nx + j], x[(i+1)*Nx + j]),
-                     (y[i*Nx + j], y[i*Nx + j]), color='b')
+            plt.plot((x[i*Ny + j], x[(i+1)*Ny + j]),
+                     (y[i*Ny + j], y[i*Ny + j]), color='b')
         elif ((i == Nx-1) and (j < Ny-1)):
-            plt.plot((x[i*Nx + j], x[i*Nx + j+1]),
-                     (y[i*Nx + j], y[i*Nx + j+1]), color='b') 
+            plt.plot((x[i*Ny + j], x[i*Ny + j+1]),
+                     (y[i*Ny + j], y[i*Ny + j+1]), color='b') 
             
 plt.scatter(x, y)
 plt.axis('equal')
-plt.savefig("grid_{}_{}.png".format(Nx,Ny))
+plt.show()
+# plt.savefig("grid_{}_{}.png".format(Nx,Ny))
