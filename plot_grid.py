3c6c1a6f8b158aa38ae0f55592637a579f786fae
Quang Ha <qth20@bu.edu>
2018-04-16 11:57:13 -0400

    Fix plotting triangles

--- plot_grid.py
+++ plot_grid.py
@@ -11,22 +11,7 @@ x, y = data[1:, 0], data[1:, 1]
 
 # Draw lines on left subplot
 plt.subplot(121)
-for i in range(Nx):
-    for j in range(Ny):
-        if ((i < Nx-1) and (j < Ny-1)):
-            plt.plot((x[i*Ny + j], x[(i+1)*Ny + j]),
-                     (y[i*Ny + j], y[(i+1)*Ny + j]), color='b')
-            plt.plot((x[i*Ny + j], x[(i+1)*Ny + (j+1)]),
-                     (y[i*Ny + j], y[(i+1)*Ny + (j+1)]), color='b')
-            plt.plot((x[i*Ny + j], x[i*Ny + (j+1)]),
-                     (y[i*Ny + j], y[i*Ny + (j+1)]), color='b')
-        elif ((i < Nx-1) and (j == Ny-1)):
-            plt.plot((x[i*Ny + j], x[(i+1)*Ny + j]),
-                     (y[i*Ny + j], y[i*Ny + j]), color='b')
-        elif ((i == Nx-1) and (j < Ny-1)):
-            plt.plot((x[i*Ny + j], x[i*Ny + j+1]),
-                     (y[i*Ny + j], y[i*Ny + j+1]), color='b')
-
+plt.triplot(a[:, 0], a[:, 1])
 plt.scatter(x, y)
 plt.axis('equal')
 plt.axis('off')
