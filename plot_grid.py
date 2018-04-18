fcdc72f391ea77cc9b8c4ee19f37563d30daead4
Quang Ha <qth20@bu.edu>
2018-04-18 12:36:24 -0400

    Fix some plotting

--- plot_grid.py
+++ plot_grid.py
@@ -18,10 +18,12 @@ plt.axis('off')
 
 # Then plot the eigenvalues
 plt.subplot(122)
+# Ratio offset
+ratio = np.sqrt(3)
 eigs_data = np.loadtxt("eigVals.dat", skiprows=1)
-plt.scatter(np.linspace(1, len(eigs_data), len(eigs_data)), eigs_data)
+plt.scatter(np.linspace(1, len(eigs_data), len(eigs_data)),
+            ratio*eigs_data[::-1])
 plt.xlabel("Eigennumber")
 plt.ylabel("Eigenvalues")
-
 # Save figure
 plt.savefig("grid_{}_{}.png".format(Nx,Ny))
